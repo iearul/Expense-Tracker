b30bc53a6747405d6408b7fc2588fcf651042478
Md Iearulialam <iearul.abid@gmail.com>
2022-08-29 00:18:45 +0200

    Expense graph chart designed

---
+++ client/src/components/Labels.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const obj = [
+    {
+        type: "Saving",
+        color: '#f9c74f',
+        percent: 45
+    },
+    {
+        type: "Investment",
+        color: '#287828',
+        percent: 15
+    },
+    {
+        type: "Expense",
+        color: '#144270',
+        percent: 40
+    }
+]
+
+export default function Labels() {
+    return (
+        <>
+            {obj.map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>)}
+        </>
+    )
+}
+
+
+function LabelComponent({ data }) {
+    if (!data) return <></>;
+    return (
+        <div className="labels flex justify-between">
+            <div className="flex gap-2">
+                <div className='w-2 h-2 rounded py-3' style={{ background: data.color ?? '#f9c74f' }}></div>
+                <h3 className='text-md'>{data.type ?? ''}</h3>
+            </div>
+            <h3 className='font-bold'>{Math.round(data.percent) ?? 0}%</h3>
+        </div>
+    )
+}
